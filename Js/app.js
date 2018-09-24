e4da5072c7648adccb238ab5775b649c42fdbfc4
Bill <ceriustech@gmail.com>
2018-09-24 11:54:53 -0500

    Changes made to multiple files.

--- Js/app.js
+++ Js/app.js
@@ -1,3 +1,6 @@
+const Router = ReactRouterDOM.BrowserRouter;
+const NavLink = ReactRouterDOM.NavLink;
+const Route = ReactRouterDOM.Route; 
 ReactDOM.render(<App />, document.querySelector('#root'));
 
 
@@ -11,7 +14,7 @@ function SidePanel() {
             </div>
             <div className="navList">
                 <p className="actionItem"><i className="fas fa-th-large"></i><a className="active actionItem" href="#home">Dashboard</a></p>
-                <p className="actionItem"><i className="fas fa-home"></i><a className="active actionItem" href="#home">Properties</a></p>
+                <p className="actionItem"><i className="fas fa-home"></i><NavLink className="active actionItem" to="/properties">Properties</NavLink></p>
                 <p className="actionItem"><i className="fas fa-user"></i><a className="actionItem" href="#news">Tenants</a></p>
                 <p className="actionItem"><i className="fas fa-angle-double-right"></i><a className="actionItem" href="#about">Transactions</a></p>
                 <p className="actionItem"><i className="fas fa-sticky-note"></i><a className="actionItem" href="#contact">Ledger</a></p>
@@ -31,7 +34,6 @@ function SidePanel() {
 //</Side Panel Navigation>
 
 //<Content Area Dashboard>
-
 function ContentArea() {
    return(
     <div className="content-area">
@@ -47,65 +49,66 @@ function ContentArea() {
                 <div className="tool-wrapper"><span className="left-border"></span><a href="#"><i className="fas fa-cog tool"></i></a></div>
             </div>
         </div>
-        <section id="dashboard-info" className="">
-            <div className="dashboard-grid-container">
-                <div className="transactions">
-                    <div className="border">
-                        <i className="fas fa-angle-double-right"></i>
-                        <h1>Transactions</h1>
-                    </div>
-                    
-                </div> 
-                <div className="properties">
-                    <div className="border">
-                        <i className="fas fa-home"></i>                                         <h1>Properties</h1>
-                    </div>
-                
-                </div>
-                <div className="tenants">
-                    <div className="border">
-                        <i className="fas fa-user"></i>
-                        <h1>Tenants</h1>
-                    </div>
-                    
-                </div>  
-                <div className="ledger">
-                    <div className="border">
-                        <i className="fas fa-sticky-note"></i>
-                        <h1>Ledger</h1>
-                    </div>
-                </div>
-                <div className="messages">
-                    <div className="border">
-                        <i className="fas fa-envelope-open"></i>
-                        <h1>Messages</h1>
-                    </div>
-                </div>
-            </div>    
-    </section>
       </section>
-       
-    </div>
+        
+            <div> 
+            <Route exact path = "/" component = {Dashboard}/>
+            <Route exact path = "/properties" component = {PropertyList}/>
+            <Route exact path = "/tenants" component = {PropertyList}/>
+            </div>
+        
+
+      </div>
    ); 
 }
-//</Content Area Dasboard>
 
-//</Content Area Properties>
-/*function ContentAreaProperties() {
-   return(
-    <div className="content-area">
-        <section className="header-section">
-        <div className="search-section">
-            <i className="fas fa-search"></i>
-            <div className="search-container">
-                <input type="text" placeholder="Search.."/>
-        </div>
-            <div className="header-section-tools">
-                <div className="tool-wrapper"><span className="left-border"></span><a href="#"><i className="fas fa-bell tool"></i></a></div>
-                <div className="tool-wrapper"><span className="left-border"></span><a href="#"><i className="far fa-question-circle tool"></i></a></div>
-                <div className="tool-wrapper"><span className="left-border"></span><a href="#"><i className="fas fa-cog tool"></i></a></div>
+//Dashboard Section
+
+function Dashboard() {
+    return(
+    <section className="dashboard-info">
+        <div className="dashboard-grid-container">
+            <div className="transactions">
+                <div className="border">
+                    <i className="fas fa-angle-double-right"></i>
+                    <h1>Transactions</h1>
+                </div>
+                
+            </div> 
+            <div className="properties">
+                <div className="border">
+                    <i className="fas fa-home"></i>
+                    <h1>Properties</h1>
+                </div>
+            
             </div>
-        </div>
+            <div className="tenants">
+                <div className="border">
+                    <i className="fas fa-user"></i>
+                    <h1>Tenants</h1>
+                </div>
+                
+            </div>  
+            <div className="ledger">
+                <div className="border">
+                    <i className="fas fa-sticky-note"></i>
+                    <h1>Ledger</h1>
+                </div>
+            </div>
+            <div className="messages">
+                <div className="border">
+                    <i className="fas fa-envelope-open"></i>
+                    <h1>Messages</h1>
+                </div>
+            </div>
+        </div>    
+    </section>
+    );
+}
+
+function PropertyList() {
+   return(
+
         <section className="property-info">
             <div className="property-grid-container">
                 <div class="property-1 property-card">
@@ -114,7 +117,7 @@ function ContentArea() {
                         <h1>555 E Ave</h1>
                     </div>
                     <div class="property-profile">
-                        <div class="property-profile-pic"><img src="Img/house-image-1.jpg"></div>
+                        <div class="property-profile-pic"><img src="Img/house-image-1.jpg"/></div>
                     </div>
                     <p><i class="fas fa-dollar-sign"></i>1,250</p>
                     <p><i class="fas fa-bed"></i> <span>2bd 1,200 sqft+</span></p>
@@ -122,22 +125,23 @@ function ContentArea() {
                 </div> 
             </div>
         </section>
-        </section>
-    </div>
    ); 
-}*/
+}
 //</Content Area Properties>
 
 //<Render Dashboard Section>
-
 function App() {
     return (
         <div className="main">
-            <div className="sidebar-navigation">
+        <Router>
+        <div>
+        <div className="sidebar-navigation">
                 <SidePanel/>
             </div>
                 <ContentArea/>
-        </div>     
+        </div>
+        </Router>
+        </div>      
     );
 }
 //</Render Dashboard Section>
