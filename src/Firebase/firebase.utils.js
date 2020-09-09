cc6737d83f239579ebb64adb75daa316e4749728
Bill <ceriustech@gmail.com>
2020-09-09 08:20:14 -0500

    Added firebase to the app.

---
+++ src/Firebase/firebase.utils.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app";
+
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyCes847FWVRY73dj1XKemXTmO9KloMbyOU",
+  authDomain: "honyst-db.firebaseapp.com",
+  databaseURL: "https://honyst-db.firebaseio.com",
+  projectId: "honyst-db",
+  storageBucket: "honyst-db.appspot.com",
+  messagingSenderId: "787406460861",
+  appId: "1:787406460861:web:0129c9b1eaa62f224524d4",
+  measurementId: "G-RLLNX69ZV3",
+};
