09b12941e1eb559216806c57383fccb68b09736e
Bill <ceriustech@gmail.com>
2020-09-15 22:35:03 -0500

    Added card components for the dashboard page.

---
+++ src/components/Dashboard-Site-Data.js/ledger-site-dboard-data.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+const LedgerCardLayout = styled.div`
+  border-radius: 10px;
+  box-shadow: 0px 0px 17px 4px rgba(217, 209, 255, 1);
+  width: 500px;
+  height: 250px;
+`;
+
+const LedgerCardData = () => {
+  return (
+    <LedgerCardLayout>
+      <h1>Ledger Card Data</h1>
+    </LedgerCardLayout>
+  );
+};
+
+export default LedgerCardData;
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,17 +1,34 @@
 import React from "react";
-//import styled from "styled-components";
+import styled from "styled-components";
+
+import LedgerCardData from "../components/Dashboard-Site-Data.js/ledger-site-dboard-data";
 
 // this should be a dashboard component per the requirements
 // i'd assume it is connected to different data so lets keep a top level state for it all.
 // add widgets and such to visualize the data
 // widgets can be removed and reusable possibly
 
+const DashBoardLayout = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+`;
+
+const DashboardContent = styled.div`
+  margin-top: 25px;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-auto-rows: repeat(3, 150px);
+  grid-gap: 10px;
+`;
+
 function Dashboard({ children }) {
   return (
-    <>
+    <DashBoardLayout>
       <h1>Dashboard</h1>
+      <LedgerCardData />
       {children}
-    </>
+    </DashBoardLayout>
   );
 }
 export default Dashboard;
